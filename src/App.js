1b1eaec46e462bb4de919ecb12df82233c8b42e7
akebrake <ake.ek@live.com>
2023-03-14 22:01:18 +0100

    Setup app for testing CSS

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './styles.css'
+
+function App() {
+  return (
+    <div>
+      <div>Hello world</div>
+      <p className='text'>Paragraph 1</p>
+    </div>
+  );
+}
+
+export default App;
