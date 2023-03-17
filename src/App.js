7a9fc455a113d63909d8b564e73d25cb3dc29de9
akebrake <ake.ek@live.com>
2023-03-17 17:47:31 +0100

    testing styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './styles.css'
+import styled from 'styled-components';
+
+const Button = styled.button`
+  border: 0;
+  background: none;
+  background-color: orange;
+  cursor: pointer;
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+`
+
 
 function App() {
   return (
     <div>
       <div>Hello world</div>
       <p className='text'>Paragraph 1</p>
+      <Button>Hello</Button>
     </div>
   );
 }
